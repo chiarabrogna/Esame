f38469ae876a5bc74e76b4fbf256a8262ffb4016
chiar <chiar@100.126.7.94>
2019-12-13 13:50:05 +0100

    primo commit

--- src/it/unina/esame/entity/corso.java
+++ src/it/unina/esame/entity/corso.java
@@ -1,5 +1,34 @@
 package it.unina.esame.entity;
 
 public class corso {
+	protected String NomeCorso;
+	protected docente NomeDocente;
+	protected int CFU;
+
+public corso() { //COSTRUTTORE SENZA PARAMETRI
+	this.NomeCorso="";
+	this.NomeDocente= new docente();
+	this.CFU=0;
+	
+}
+
+public corso(String Corso,docente Docente,int _CFU) {//COSTRUTTORE CON PARAMETRI 
+	this.NomeCorso= new String(Corso);
+	NomeDocente=Docente;
+	CFU=_CFU;
+}
+
+public String getNomeCorso() {
+	return NomeCorso;
+}
+
+public void setNomeCorso(String Corso) {
+	NomeCorso=Corso;
+}
+
+public docente getdocente() {
+	return NomeDocente;
+	
+}
 
 }
--- src/it/unina/esame/entity/docente.java
+++ src/it/unina/esame/entity/docente.java
@@ -1,5 +1,18 @@
 package it.unina.esame.entity;
 
 public class docente {
-
+	protected String NomeDocente;
+	
+	public docente() {
+		this.NomeDocente="";
+	}
+	public docente(String Docente) {
+		this.NomeDocente= new String(Docente);
+	}
+	public String getNomeDocente() {
+		return NomeDocente;
+	}
+	public void setNomeDocente(String Docente) {
+		NomeDocente=Docente;
+	}
 }
